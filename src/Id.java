50bc218f78ac9aa0b2c94a7bcecbbc94f492bf74
Jake Klingler <thearrow3456@gmail.com>
2012-10-21 13:56:13 -0400

    start Id class

--- src/Id.java
+++ src/Id.java
@@ -1,5 +1,21 @@
+import java.util.Set;
+
 public class Id {
-    void ParseId(){
+
+    private String name;
+    private int val;
+    private Boolean declared;
+    private Boolean initialized;
+    private static Set<Id> idSet;
+
+    private Id(String n) {
+        name = n;
+        declared = false;
+        initialized =false;
+        val = -1;
+    }
+
+    public static void ParseId(){
 
     }
 
