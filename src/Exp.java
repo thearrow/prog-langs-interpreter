dcdfa9bf1b883e837c344794c0f0348270036789
Jake Klingler <thearrow3456@gmail.com>
2012-10-21 22:32:21 -0400

    refactor Exp

--- src/Exp.java
+++ src/Exp.java
@@ -1,61 +1,61 @@
 public class Exp {
-    private Fac fac;
-
-    private enum PM {
-        PLUS,
-        MINUS,
-        NONE
-    }
-    private PM pm;
-    private Exp exp;
+    private Fac     fac;
+    private Exp     exp;
+    private Boolean plus;
+    private Boolean minus;
 
     public Exp() {
-        fac = null;
-        exp = null;
-        pm = PM.NONE;
+        fac   = null;
+        exp   = null;
+        plus  = false;
+        minus = false;
     }
 
-    void ParseExp(){
+    void ParseExp() {
         fac = new Fac();
         fac.ParseFac();
 
         String token = Tokenizer.INSTANCE.getToken();
-        if(token.equals("+")){
-            pm = PM.PLUS;
-            Tokenizer.INSTANCE.skipToken();
+
+        if (token.equals("+")) {
+            plus = true;
+            Tokenizer.INSTANCE.skipToken();    // +
             exp = new Exp();
             exp.ParseExp();
-        }
-        else if(token.equals("-")){
-            pm = PM.MINUS;
-            Tokenizer.INSTANCE.skipToken();
+        } else if (token.equals("-")) {
+            minus = true;
+            Tokenizer.INSTANCE.skipToken();    // -
             exp = new Exp();
             exp.ParseExp();
         }
     }
 
-    void PrintExp(){
-        if(fac != null) fac.PrintFac();
-        if(pm == PM.PLUS) System.out.print("+");
-        if(pm == PM.MINUS) System.out.print("-");
-        if(exp != null) exp.PrintExp();
-    }
-
-    void ExecExp(){
-
+    void PrintExp() {
+        if (fac != null) {
+            fac.PrintFac();
+        }
+        if (plus) {
+            System.out.print("+");
+        }
+        if (minus) {
+            System.out.print("-");
+        }
+        if (exp != null) {
+            exp.PrintExp();
+        }
     }
 
     public int getVal() {
-
         if (exp == null) {
             return fac.getVal();
         } else {
-            if (pm == PM.PLUS) {
-                return fac.getVal()+exp.getVal();
-            } else if (pm == PM.MINUS) {
-                return fac.getVal()-exp.getVal();
+            if (plus) {
+                return fac.getVal() + exp.getVal();
+            } else if (minus) {
+                return fac.getVal() - exp.getVal();
             }
         }
+
         return 0;
     }
 }
