6cd603972cc999423bffb216f4b2a909a03b9fe1
Jake Klingler <thearrow3456@gmail.com>
2012-10-21 22:13:39 -0400

    support for multiple negated conditions

--- src/Comp.java
+++ src/Comp.java
@@ -2,13 +2,11 @@ public class Comp {
     private Op     op1;
     private Op     op2;
     private CompOp compOp;
-    private Boolean negated;
 
     public Comp() {
         op1    = null;
         op2    = null;
         compOp = null;
-        negated = false;
     }
 
     void ParseComp() {
@@ -23,16 +21,11 @@ public class Comp {
     }
 
     void PrintComp() {
-        if(negated) System.out.print("!");
         System.out.print("(");
         op1.PrintOp();
         compOp.PrintCompOp();
         op2.PrintOp();
         System.out.print(")");
-    }
-
-    void negate(){
-        negated = true;
     }
 
     public boolean EvalComp() {
@@ -50,9 +43,6 @@ public class Comp {
             result = op1.getVal() <= op2.getVal();
         else if(compOp.getOp() == CompOp.CompSymb.GREATEREQ)
             result = op1.getVal() >= op2.getVal();
-
-        if(negated)
-            result = !result;
 
         return result;
     }
--- src/Cond.java
+++ src/Cond.java
@@ -2,6 +2,18 @@ public class Cond {
     private Comp comp;
     private Cond c1;
     private Cond c2;
+    private Boolean and;
+    private Boolean or;
+    private Boolean negated;
+
+    public Cond() {
+        comp = null;
+        c1 = null;
+        c2 = null;
+        and = false;
+        or = false;
+        negated = false;
+    }
 
     void ParseCond(){
         String token = Tokenizer.INSTANCE.getToken();
@@ -12,25 +24,61 @@ public class Cond {
         }
         else if (token.equals("!")) {
             Tokenizer.INSTANCE.skipToken(); //!
-            comp = new Comp();
-            comp.ParseComp();
-            comp.negate();
+            negated = true;
+            c1 = new Cond();
+            c1.ParseCond();
         }
         else if (token.equals("[")) {
             Tokenizer.INSTANCE.skipToken(); //[
-            //TODO: Handle [
             c1 = new Cond();
             c1.ParseCond();
 
-            System.out.println(Tokenizer.INSTANCE.getToken());
+            token = Tokenizer.INSTANCE.getToken();
+            if (token.equals("&&")) {
+                Tokenizer.INSTANCE.skipToken(); //&&
+                and = true;
+                c2 = new Cond();
+                c2.ParseCond();
+                Tokenizer.INSTANCE.skipToken(); //]
+            } else if (token.equals("||")) {
+                Tokenizer.INSTANCE.skipToken(); //||
+                or = true;
+                c2 = new Cond();
+                c2.ParseCond();
+                Tokenizer.INSTANCE.skipToken();//]
+            }
         }
     }
 
-    void PrintCond(){
-        comp.PrintComp();
+    void PrintCond() {
+        if(negated) System.out.print("!");
+        if (and) {
+            System.out.print("[");
+            c1.PrintCond();
+            System.out.print("&&");
+            c2.PrintCond();
+            System.out.print("]");
+        } else if (or) {
+            System.out.print("[");
+            c1.PrintCond();
+            System.out.print("||");
+            c2.PrintCond();
+            System.out.print("]");
+        } else{
+            if(c1 != null) c1.PrintCond();
+            else comp.PrintComp();
+        }
+
     }
 
-    public boolean EvalCond(){
-        return comp.EvalComp();
+    public boolean EvalCond() {
+        if (and) {
+            return (c1.EvalCond() && c2.EvalCond());
+        } else if (or) {
+            return (c1.EvalCond() || c2.EvalCond());
+        } else {
+            if (c1 != null) return c1.EvalCond();
+            else return comp.EvalComp();
+        }
     }
 }
